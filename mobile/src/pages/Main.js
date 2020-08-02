a10f35799c9b368ad5e64d870cc79d5150041063
mfmoreira <mfmoreira7@gmail.com>
2020-08-02 18:36:26 -0300

    tag image e marker

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync,
@@ -35,12 +35,31 @@ function Main() {
     return null;
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />;
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -3.6066109, longitude: -38.9663866 }}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri:
+              'https://avatars1.githubusercontent.com/u/55352687?s=400&u=9616b0232070a400c3c602212b55d9acc4a055e6&v=4',
+          }}
+        ></Image>
+      </Marker>
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
   map: {
     flex: 1,
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF',
   },
 });
 
